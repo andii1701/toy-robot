e790654c16a1294a5dfad337049bb7e4d4de646c
Andrew Jones <andrew.jones@c02s85p5g8wp.win.int.realestate.com.au>
2018-02-15 18:45:34 +1100

    extend main function to read commands.txt file

--- src/toy-robot.kt
+++ src/toy-robot.kt
@@ -1,5 +1,16 @@
 import java.io.File
+import java.io.FileNotFoundException
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val commands = mutableListOf<String>()
+    val filename = "commands.txt"
+    try {
+        File(filename).useLines { lines -> lines.forEach { commands.add(it) } }
+    }
+    catch(e: FileNotFoundException)  {
+        println("Error $filename not found.")
+        exitProcess(1)
+    }
+    print(commands)
 }
