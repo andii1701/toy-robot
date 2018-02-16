37ee92d6bda3bef4a7df474cf71c0fb045293319
Andrew Jones <andrew.jones@c02s85p5g8wp.win.int.realestate.com.au>
2018-02-16 20:56:57 +1100

    Extended TableTop class by adding onTable method and a yLength value

--- src/main/kotlin/toyrobot/TableTop.kt
+++ src/main/kotlin/toyrobot/TableTop.kt
@@ -1,6 +1,16 @@
 package toyrobot
 
-class TableTop(xLength: Int) {
+class TableTopException(override var message:String): Exception(message)
+
+
+class TableTop(xLength: Int, yLength: Int) {
     val xLength = xLength
+    val yLength = yLength
+
+    init {
+        if (xLength < 0 || yLength < 0) throw TableTopException("Table dimensions must not be negative")
+    }
+
+    fun onTable(x: Int, y: Int): Boolean = x in 0 until xLength && y in 0 until yLength
 
 }
--- src/test/kotlin/toyrobot/TableTopTest.kt
+++ src/test/kotlin/toyrobot/TableTopTest.kt
@@ -1,11 +1,33 @@
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldThrow
 import io.kotlintest.specs.StringSpec
 import toyrobot.TableTop
+import toyrobot.TableTopException
 
 class TableTopTests : StringSpec() {
     init {
-        "TableTop(length=5).length should be 5"  {
-            TableTop(xLength=5).xLength shouldBe 5
+        "TableTop(xLength=5, yLength=5) should set xLength to 5 and yLength to 6"  {
+            val t = TableTop(xLength=5, yLength=6)
+            t.xLength shouldBe 5
+            t.yLength shouldBe 6
+        }
+        "TableTop() xLength and yLength values should not be negative"  {
+            shouldThrow<TableTopException>  {
+                TableTop(xLength=-1, yLength=0)
+            }
+            shouldThrow<TableTopException>  {
+                TableTop(xLength=0, yLength=-1)
+            }
+        }
+        "TableTop(...).onTable(x, y) method should return true if x,y are coordinates are on the table" {
+            val t = TableTop(xLength=5, yLength=5)
+            t.onTable(0,0) shouldBe true
+            t.onTable(4,4) shouldBe true
+            t.onTable(4,5) shouldBe false
+            t.onTable(5,4) shouldBe false
+            t.onTable(-1,0) shouldBe false
+            t.onTable(0,-1) shouldBe false
+            t.onTable(-1,-1) shouldBe false
         }
 
     }
