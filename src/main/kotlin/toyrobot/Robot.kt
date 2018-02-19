e9c45391c59a034f9cae065222fde14789cf32a3
Andrew Jones <andrew.jones@c02s85p5g8wp.win.int.realestate.com.au>
2018-02-19 15:56:13 +1100

    Made commandParser validate fun now uses a single regex

--- src/main/kotlin/toyrobot/Robot.kt
+++ src/main/kotlin/toyrobot/Robot.kt
@@ -5,15 +5,15 @@ class Robot(private var tableTop: TableTop)  {
 
     private var v: SimpleVector? = null
 
-    fun getVec(): SimpleVector? = v?.copy()
+    fun getVec(): SimpleVector? = this.v?.copy()
 
     fun placed(): Boolean = v != null
 
-    fun report(): String? = if (this.placed()) "${v?.x},${v?.y} and ${v?.heading}" else null
+    fun report(): String? = if (this.placed()) "${this.v?.x},${this.v?.y} and ${this.v?.heading}" else null
 
     fun place(v: SimpleVector) { if (tableTop.isOn(v.x, v.y)) this.v = v.copy() }
 
-    fun turn(turnDirection: TurnDirection) { v?.turn(turnDirection) }
+    fun turn(turnDirection: TurnDirection) { this.v?.turn(turnDirection) }
 
     fun move() {
         if (!this.placed()) return
--- src/main/kotlin/toyrobot/commandParser/commandParser.kt
+++ src/main/kotlin/toyrobot/commandParser/commandParser.kt
@@ -8,6 +8,9 @@ class CommandsParsingException(override var message:String): Exception(message)
 class NoCommandsException(override var message:String): Exception(message)
 
 
+val placeCmdRgex = Regex("""PLACE \d+,\d+,(NORTH|SOUTH|EAST|WEST)""")
+val commandRegex = Regex("""(REPORT|MOVE|LEFT|RIGHT|$placeCmdRgex)""")
+
 fun validateCommands(commands: List<String>) {
     if (commands.isEmpty()) throw NoCommandsException("List of commands is empty")
     commands.forEach {
@@ -15,17 +18,9 @@ fun validateCommands(commands: List<String>) {
     }
 }
 
-fun isValidPlaceCommand(command: String):
-        Boolean = Regex("""PLACE \d+,\d+,(NORTH|SOUTH|EAST|WEST)""").matches(command)
+fun isValidPlaceCommand(command: String): Boolean = placeCmdRgex.matches(command)
 
-fun isValidCommand(command: String): Boolean  {
-    if (isValidPlaceCommand(command)) { return true }
-
-    return when (command) {
-        "MOVE", "LEFT", "RIGHT", "REPORT" -> true
-        else -> false
-        }
-}
+fun isValidCommand(command: String): Boolean = commandRegex.matches(command)
 
 fun parsePlaceCommand(cmd: String): SimpleVector  {
     if (!isValidPlaceCommand(cmd)) throw CommandsParsingException("Error could not parse command: $cmd")
