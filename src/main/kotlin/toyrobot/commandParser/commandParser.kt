cac35cc5ee72a6db82bbabac1901a8e38789ffdd
Andrew Jones <andrew.jones@c02s85p5g8wp.win.int.realestate.com.au>
2018-02-19 16:31:20 +1100

    Removed split of place command and replaced it with a regex match group

--- src/main/kotlin/toyrobot/commandParser/commandParser.kt
+++ src/main/kotlin/toyrobot/commandParser/commandParser.kt
@@ -8,23 +8,25 @@ class CommandsParsingException(override var message:String): Exception(message)
 class NoCommandsException(override var message:String): Exception(message)
 
 
-val placeCmdRgex = Regex("""PLACE \d+,\d+,(NORTH|SOUTH|EAST|WEST)""")
-val commandRegex = Regex("""(REPORT|MOVE|LEFT|RIGHT|$placeCmdRgex)""")
+val placeCmdRegex = Regex("""PLACE (?<x>\d+),(?<y>\d+),(?<heading>NORTH|SOUTH|EAST|WEST)""")
+val commandRegex = Regex("""(REPORT|MOVE|LEFT|RIGHT|$placeCmdRegex)""")
 
 fun validateCommands(commands: List<String>) {
     if (commands.isEmpty()) throw NoCommandsException("List of commands is empty")
+
     commands.forEach {
         if (!isValidCommand(it)) { throw CommandsParsingException("Error could not parse command: '$it'") }
     }
 }
 
-fun isValidPlaceCommand(command: String): Boolean = placeCmdRgex.matches(command)
+fun isValidPlaceCommand(command: String): Boolean = placeCmdRegex.matches(command)
 
 fun isValidCommand(command: String): Boolean = commandRegex.matches(command)
 
 fun parsePlaceCommand(cmd: String): SimpleVector  {
     if (!isValidPlaceCommand(cmd)) throw CommandsParsingException("Error could not parse command: $cmd")
-        // TODO convert to regex group
-    val s = cmd.split(',')
-    return SimpleVector(s[0].removePrefix("PLACE ").toInt(),s[1].toInt(), Heading.valueOf(s[2]))
+
+    val l = placeCmdRegex.matchEntire(cmd)!!.groupValues
+
+    return SimpleVector(l[1].toInt(),l[2].toInt(), Heading.valueOf(l[3]))
 }
